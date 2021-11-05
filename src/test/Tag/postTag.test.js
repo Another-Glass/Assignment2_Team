f4b0f293798fbf2ef4ee2457a50f0bc3ab7750e6
epitone <epitoneplus@gmail.com>
2021-11-06 01:10:23 +0900

    [fix]: 태그 생성 테스트 코드 수정

--- src/test/Tag/postTag.test.js
+++ src/test/Tag/postTag.test.js
@@ -1,65 +1,65 @@
-// // const app = require('../../app');
-// const supertest = require('supertest');
-// const responseMessage = require('../../globals/responseMessage');
-// const statusCode = require('../../globals/statusCode');
-// const routes = require('../../globals/routes');
-// const faker = require('faker');
-// const dotenv = require('dotenv');
-// dotenv.config();
+// const app = require('../../app');
+const supertest = require('supertest');
+const responseMessage = require('../../globals/responseMessage');
+const statusCode = require('../../globals/statusCode');
+const routes = require('../../globals/routes');
+const faker = require('faker');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// const host = `http://localhost:${process.env.PORT}`;
-// const testClient = supertest(host);
+const host = `${process.env.HOST}`;
+const testClient = supertest(host);
 
-// const adminUser = `${process.env.ADMIN_USER}`;
-// const adminPassword = `${process.env.ADMIN_PASSWORD}`;
+const adminUser = `${process.env.ADMIN_USER}`;
+const adminPassword = `${process.env.ADMIN_PASSWORD}`;
 
-// let token = '';
+let token = '';
 
-// describe('토큰 생성하기', () => {
-//   test('토큰 생성 성공', async () => {
-//     const res = await testClient
-//       .post(`${routes.user}${routes.signin}`)
-//       .send(
-//         {
-//           "email" : adminUser,
-//           "password" : adminPassword
-//         }
-//       )
-//     expect(res.status).toBe(statusCode.OK)
-//     expect(res.body.success).toBe(true)
-//     expect(res.body.message).toBe(responseMessage.LOGIN_SUCCESS)
-//     token = res.body.data.accessToken
-//   })
-// })
+describe('토큰 생성하기', () => {
+  test('토큰 생성 성공', async () => {
+    const res = await testClient
+      .post(`${routes.token}`)
+      .send(
+        {
+          "email" : adminUser,
+          "password" : adminPassword
+        }
+      )
+    expect(res.status).toBe(statusCode.OK)
+    expect(res.body.success).toBe(true)
+    expect(res.body.message).toBe(responseMessage.LOGIN_SUCCESS)
+    token = res.body.data.accessToken
+  })
+})
 
-// // describe('태그 생성하기', () => {
-// //   test('태그 생성 성공', async () => {
-// //       const res = await testClient
-// //         .post(`${routes.tag}`)
-// //         .set('token', token)
-// //         .send(
-// //           {
-// //             "type": faker.lorem.word(),
-// //             "name": faker.lorem.text(),
-// //           }
-// //         )
-// //       expect(res.status).toBe(statusCode.CREATED)
-// //       expect(res.body.success).toBe(true)
-// //       expect(res.body.message).toBe(responseMessage.CREATE_TAG_SUCCESS)
-// //     })
+describe('태그 생성하기', () => {
+  test('태그 생성 성공', async () => {
+      const res = await testClient
+        .post(`${routes.tag}`)
+        .set('Authorization', token)
+        .send(
+          {
+            "type": faker.lorem.word(),
+            "name": faker.lorem.text(),
+          }
+        )
+      expect(res.status).toBe(statusCode.CREATED)
+      expect(res.body.success).toBe(true)
+      expect(res.body.message).toBe(responseMessage.CREATE_TAG_SUCCESS)
+    })
   
-// //     test('태그 생성 입력값 누락 ', async () => {
-// //       const res = await testClient
-// //         .post(`${routes.tag}`)
-// //         .set('token', token)
-// //         .send(
-// //           {
-// //             "title": "testtest"
-// //           }
-// //         )
-// //       expect(res.status).toBe(statusCode.BAD_REQUEST)
-// //       expect(res.body.success).toBe(false)
-// //       expect(res.body.message).toBe(responseMessage.NULL_VALUE)
-// //     })
-// // })
+    test('태그 생성 입력값 누락 ', async () => {
+      const res = await testClient
+        .post(`${routes.tag}`)
+        .set('Authorization', token)
+        .send(
+          {
+            "type": faker.lorem.word()
+          }
+        )
+      expect(res.status).toBe(statusCode.BAD_REQUEST)
+      expect(res.body.success).toBe(false)
+      expect(res.body.message).toBe(responseMessage.NULL_VALUE)
+    })
+})
   
