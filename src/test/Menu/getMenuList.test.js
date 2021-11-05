0cdb9df38ac8300e483997f207f731458b6ed6e9
epitone <epitoneplus@gmail.com>
2021-11-06 00:30:28 +0900

    [fix]: 전체 메뉴 조회 테스트 코드 수정

--- src/test/Menu/getMenuList.test.js
+++ src/test/Menu/getMenuList.test.js
@@ -1,30 +1,22 @@
-// // const app = require('../../app');
-// const supertest = require('supertest');
-// const responseMessage = require('../../globals/responseMessage');
-// const statusCode = require('../../globals/statusCode');
-// const routes = require('../../globals/routes');
-// const faker = require('faker');
-// const dotenv = require('dotenv');
-// dotenv.config();
+// const app = require('../../app');
+const supertest = require('supertest');
+const responseMessage = require('../../globals/responseMessage');
+const statusCode = require('../../globals/statusCode');
+const routes = require('../../globals/routes');
+const faker = require('faker');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// const host = `http://localhost:${process.env.PORT}`;
-// const testClient = supertest(host);
+const host = `${process.env.HOST}`;
+const testClient = supertest(host);
 
-// describe('전체 메뉴 조회하기', () => {
-//   test('전체 메뉴 조회 성공', async () => {
-//       const res = await testClient
-//         .get(`${routes.post}?offset=0&limit=5`)
-//       expect(res.status).toBe(statusCode.OK)
-//       expect(res.body.success).toBe(true)
-//       expect(res.body.message).toBe(responseMessage.READ_POST_SUCCESS)
-//       expect(typeof (res.body.data)).toBe('object')
-//     })
-
-//   test('메뉴가 없어서 메뉴 조회 불가', async () => {
-//       const res = await testClient
-//         .get(`${routes.post}`)
-//       expect(res.status).toBe(statusCode.BAD_REQUEST)
-//       expect(res.body.success).toBe(false)
-//       expect(res.body.message).toBe(responseMessage.NULL_VALUE)
-//     })
-// })
+describe('전체 메뉴 조회하기', () => {
+  test('전체 메뉴 조회 성공', async () => {
+      const res = await testClient
+        .get(`${routes.menu}?page=1`)
+      expect(res.status).toBe(statusCode.OK)
+      expect(res.body.success).toBe(true)
+      expect(res.body.message).toBe(responseMessage.READ_MENU_SUCCESS)
+      expect(typeof (res.body.data)).toBe('object')
+    })
+})
